459800e4bde2e44d0883179c9a12c345156b7a0e
Tao2564548730 <2564548730@qq.com>
2019-07-03 21:36:11 +0800

    Update main.cpp

--- New world/main.cpp
+++ New world/main.cpp
@@ -1,18 +1,34 @@
-//
-//  main.cpp
-//  New world
-//
-//  Created by W20181105881 on 2019/6/13.
-//  Copyright © 2019 W20181105881. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main() {
-    // insert code here...
-    int a,b;
-    cin>>a>>b;
-    int c=a*b;
-    cout<<c<<endl;
+ifstream sin("/Users/WT20181105881/Desktop/in.txt");
+
+int main(){
+    int n;
+    sin>>n;
+    int f=0;
+    int j=0;
+    int l=0;
+    char a[30];
+    do{
+        f++;
+        int x=n%10;
+        l+=x;
+        n/=10;
+        if(f==1){
+            for(int i=x;i>0;i--)
+                a[j++]=i+'0';
+        }
+        if(f==2)
+            for(int i=0;i<x;i++)
+                a[j++]='s';
+        if(f==3)
+            for(int i=0;i<x;i++)
+                a[j++]='b';
+    }while(n!=0);
+    for(int i=0;l/2+1;i++)
+        a[i]=a[l-i-1];
+    cout<<a<<endl;
 }
